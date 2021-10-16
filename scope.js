25592d2a5e328bd82f739a3f3de642d8dd4e1827
martinthetechie <martinthetechie@gmail.com>
2021-10-17 00:55:58 +0800

    Adding the files for Core Concepts of Javascript

---
+++ scope.js
@@ -0,0 +1,31 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/',(req,res)=>{
+//     res.send('Hannah Montanna')
+// })
+
+
+// const server =app.listen(3000, ()=>console.log('Test'))
+// process.on("SIGTERM",()=>{
+//     server.close(()=>{
+//         console.log('Service is terminating')
+//     })
+// })
+
+
+const oranges = ['orange', 'orange']
+const apples = ['just one apple']
+oranges.forEach(fruit => {
+  console.count(fruit)
+})
+apples.forEach(fruit => {
+  console.count(fruit)
+})
+
+console.countReset('orange')
+
+oranges.forEach(fruit => {
+  console.count(fruit)
+})
+
